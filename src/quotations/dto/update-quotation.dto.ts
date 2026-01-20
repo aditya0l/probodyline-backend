ce9ee4501b030fb648ccf9ed96a9b4b8ee91bd91
aditya0l <aditya0l@users.noreply.github.com>
2026-01-20 23:51:46 +0530

    Feat: Update Quotation DTO for quoteNumber edits

--- src/quotations/dto/update-quotation.dto.ts
+++ src/quotations/dto/update-quotation.dto.ts
@@ -1,10 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateQuotationDto } from './create-quotation.dto';
-import { IsOptional, IsArray, ValidateNested } from 'class-validator';
+import { IsOptional, IsArray, ValidateNested, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
 import { CreateQuotationItemDto } from './create-quotation-item.dto';
 
 export class UpdateQuotationDto extends PartialType(CreateQuotationDto) {
+  @IsOptional()
+  @IsString()
+  quoteNumber?: string;
+
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })

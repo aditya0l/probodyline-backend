a740e4870f0a4bedc2ade5b0519c1111e384d93d
aditya0l <aditya0l@users.noreply.github.com>
2026-06-24 23:35:54 +0530

    fix: imports for factories module

--- src/factories/factories.module.ts
+++ src/factories/factories.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { FactoriesService } from './factories.service';
 import { FactoriesController } from './factories.controller';
-import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [FactoriesController],
   providers: [FactoriesService],
 })
